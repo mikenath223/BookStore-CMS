d891bf94c635c3fb3656cee1d32983a4d5f88630
viricruz <viridiana.cruz.olivera@gmail.com>
2020-04-13 13:25:21 -0700

    Add App component import

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-
-class App extends React.Component {
-
-  render() {
-    return (
-      <div>
-        <h1>Hello World!</h1>
-      </div>
-    )
-  }
-}
+import App from './components/app'
 
 
 ReactDOM.render(<App/>, document.getElementById('root'))
