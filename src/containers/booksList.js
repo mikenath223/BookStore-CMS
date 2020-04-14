f338fa2870800410d6a90e1acf13198e4bcd6d42
viricruz <viridiana.cruz.olivera@gmail.com>
2020-04-14 12:27:01 -0700

    Add connect to map state and dispatch to props to BooksList container component. Also render books from state

--- src/containers/booksList.js
+++ src/containers/booksList.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import Book from '../components/book';
 import { CREATE_BOOK, REMOVE_BOOK } from '../actions/index';
 
+// container component
 const mapDispatchToProps = dispatch => ({
   addBook: book => {
     dispatch(CREATE_BOOK(book));
@@ -16,28 +17,25 @@ const mapStateToProps = state => ({
   books: state,
 });
 
-connect(mapStateToProps, mapDispatchToProps)(App);
+const BooksList = (props) => {
+  const { books } = props.books
+ 
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Book ID</th>
+            <th>Title</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          { books.map(book => <Book key={book.title} book={book} />) }
+        </tbody>
+      </table>
+    </div>
+  );
+}
 
-const BooksList = () => (
-  <div>
-    <table>
-      <thead>
-        <tr>
-          <th>Book ID</th>
-          <th>Title</th>
-          <th>Category</th>
-        </tr>
-      </thead>
-      <tbody>
-
-        <Book book="" />
-        <Book book="" />
-        <Book book="" />
-        <Book book="" />
-        <Book book="" />
-      </tbody>
-    </table>
-  </div>
-);
-
-export { BooksList, mapDispatchToProps };
+export default connect(mapStateToProps, mapDispatchToProps)(BooksList);
