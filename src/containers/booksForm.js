095035d7de1bf3ad4109c071dac3b6c670061245
Michgolden Ukeje <michgoldennathan17@gmail.com>
2020-04-15 21:35:27 +0100

    [Feature] Implement bookForm state store of title and category

--- src/containers/booksForm.js
+++ src/containers/booksForm.js
@@ -1,31 +1,42 @@
 import React from 'react';
 
-const BookForm = () => {
-  const bookCategories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
-  return (
-    <form>
-      <fieldset className="form-field">
-        <label htmlFor="title">
-          Book Title
-          <input type="text" name="title" id="title" className="input-title" />
-        </label>
+class BookForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: '',
+      category: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
 
-      </fieldset>
-      <fieldset>
-        <label htmlFor="category">
-          Book Category
-          <select name="category" className="select-field">
-            {bookCategories.map(cat => (
-              <option key={cat} value={cat}>
-                {cat}
-              </option>
-            ))}
-          </select>
-        </label>
-      </fieldset>
-      <button type="submit">Add Book</button>
-    </form>
-  );
+  render() {
+    const bookCategories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+    return (
+      <form >
+        <fieldset className="form-field">
+          <label htmlFor="title">
+            Book Title
+          <input type="text" name="title" id="title"  className="input-title" />
+          </label>
+
+        </fieldset>
+        <fieldset>
+          <label htmlFor="category">
+            Book Category
+          <select name="category" className="select-field" >
+              {bookCategories.map(cat => (
+                <option key={cat} value={cat}>
+                  {cat}
+                </option>
+              ))}
+            </select>
+          </label>
+        </fieldset>
+        <button type="submit">Add Book</button>
+      </form>
+    );
+  }
 };
 
 export default BookForm;
