b42036423b87485ba01433c894d92e5a3f9ec4f1
Michgolden Ukeje <michgoldennathan17@gmail.com>
2020-04-15 21:36:49 +0100

    [Feature] Create handleChange method to track input event

--- src/containers/booksForm.js
+++ src/containers/booksForm.js
@@ -9,6 +9,20 @@ class BookForm extends React.Component {
     }
     this.handleChange = this.handleChange.bind(this)
   }
+  handleChange(e, elem) {
+    switch (elem) {
+      case 'title':
+        this.setState({
+          title: e.target.value
+        })
+        break;
+      default:
+        this.setState({
+          category: e.target.value
+        })
+        break;
+    }
+  }
 
   render() {
     const bookCategories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
@@ -17,14 +31,14 @@ class BookForm extends React.Component {
         <fieldset className="form-field">
           <label htmlFor="title">
             Book Title
-          <input type="text" name="title" id="title"  className="input-title" />
+          <input type="text" name="title" id="title" onChange={(e) => this.handleChange(e, 'title')} className="input-title" />
           </label>
 
         </fieldset>
         <fieldset>
           <label htmlFor="category">
             Book Category
-          <select name="category" className="select-field" >
+          <select name="category" className="select-field" onChange={(e) => this.handleChange(e, 'category')}>
               {bookCategories.map(cat => (
                 <option key={cat} value={cat}>
                   {cat}
