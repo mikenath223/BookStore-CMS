2bb330351adada3b7da4a7b2a8dfb92b58a2454c
Michgolden Ukeje <michgoldennathan17@gmail.com>
2020-04-16 18:57:25 +0100

    [Update] Change filtercategory conditional

--- src/reducers/filter.js
+++ src/reducers/filter.js
@@ -3,9 +3,7 @@ import { FILTER_BOOK } from "../actions";
 const filterCategory = (state = [], action) => {
   switch (action.type) {
     case FILTER_BOOK:
-      return [
-        ...state,
-      ].filter(bk => bk.category === action.category)
+      return Object.assign({}, state, { filter: action.category })
     default:
       return state
   }
