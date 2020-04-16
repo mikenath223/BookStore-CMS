745c38c61a3641a237b122243b76beee23273597
Michgolden Ukeje <michgoldennathan17@gmail.com>
2020-04-16 23:02:45 +0100

    [Feature] Add styling to page header component

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import BooksList from '../containers/booksList';
 import BookForm from '../containers/booksForm';
+import avatar from '../styles/icons8-user-30.png' 
 
 const App = () => (
-  <div className="container">
-    <nav>
-      <h4>LibTriXx</h4>
+  <div className="container main-container">
+    <nav className="row mt-3">
+      <ul className="nav col-6">
+        <li className="nav-item">
+          <a className="nav-link active logo mt-2" href="#"><h2>LibTrixx CMS</h2></a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link mt-3 text-dark" href="#">Books</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link mt-3 text-dark" href="#">Categories</a>
+        </li>
+      </ul>
+      <div className="col-6 pt-2">
+        <img src={avatar} className="mt-3 float-right"/>
+      </div>
     </nav>
     <BooksList />
     <BookForm />
